a5f5ef86998b41d264fcf2157e045f131ddede69
JoM02 <joxel2002@gmail.com>
2026-03-18 13:05:51 +0100

    Frontend : adding navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from "./components/Navbar.vue"
 </script>
 
 <template>
-  <HelloWorld />
+  <div>
+    <Navbar />
+    <router-view />
+  </div>
 </template>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/">Vitrine</router-link>
+  
+        <div>
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+  
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Accueil</router-link>
+            </li>
+  
+            <li class="nav-item">
+              <router-link class="nav-link" to="/clients">Clients</router-link>
+            </li>
+  
+            <li class="nav-item">
+              <router-link class="nav-link" to="/orders">Commandes</router-link>
+            </li>
+  
+          </ul>
+        </div>
+      </div>
+    </nav>
+</template>
